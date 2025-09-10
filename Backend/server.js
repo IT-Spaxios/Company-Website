0f11ea0a2573490d26916532505be3db269ec169
Mahalakshmi <mmahaece07@gmail.com>
2025-09-10 11:13:25 +0530

    port

--- Backend/server.js
+++ Backend/server.js
@@ -22,6 +22,7 @@ const app = express();
 const allowedOrigins = [
   "http://127.0.0.1:5500",
    "http://127.0.0.1:5501",
+   "http://localhost:5501",
   "http://localhost:5500",
   "http://localhost:3000",
   /\.vercel\.app$/   // ✅ allow any subdomain of vercel.app
