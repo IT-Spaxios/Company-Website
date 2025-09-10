344736b5858e6a1f7743f3c0db25894a98c63cfb
Mahalakshmi <mmahaece07@gmail.com>
2025-09-10 10:38:05 +0530

    port added

--- Backend/server.js
+++ Backend/server.js
@@ -21,6 +21,7 @@ const app = express();
 // ✅ global cors setup
 const allowedOrigins = [
   "http://127.0.0.1:5500",
+   "http://127.0.0.1:5501",
   "http://localhost:5500",
   "http://localhost:3000",
   /\.vercel\.app$/   // ✅ allow any subdomain of vercel.app
